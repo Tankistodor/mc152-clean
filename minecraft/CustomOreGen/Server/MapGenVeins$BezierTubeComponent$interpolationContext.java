3c888ab14c4b3f4b91f6d419ce614769fb4b9282
Tankistodor <tankistodor.tankistodor@gmail.com>
2013-06-25 23:33:32 +0400

    Fixed COG bug infinite loop in Veins gen

--- minecraft/CustomOreGen/Server/MapGenVeins$BezierTubeComponent$interpolationContext.java
+++ minecraft/CustomOreGen/Server/MapGenVeins$BezierTubeComponent$interpolationContext.java
@@ -56,60 +56,68 @@ class MapGenVeins$BezierTubeComponent$interpolationContext
     }
 
     public boolean advance(float var1)
-    {
-        float var2 = this.pos[0];
-        float var3 = this.pos[1];
-        float var4 = this.pos[2];
-        float var5 = this.der[0];
-        float var6 = this.der[1];
-        float var7 = this.der[2];
-        float var8 = this.radius;
+    { //TODO JOPA
+        float pX = this.pos[0];
+        float pY = this.pos[1];
+        float pZ = this.pos[2];
+        float dX = this.der[0];
+        float dY = this.der[1];
+        float dZ = this.der[2];
+        float r = this.radius;
 
         do
         {
-            float var9 = this.t + this.dt;
-            this.this$1.interpolatePosition(this.pos, var9);
-            float var10 = var2 - this.pos[0];
-            float var11 = var3 - this.pos[1];
-            float var12 = var4 - this.pos[2];
-            float var13 = var10 * var10 + var11 * var11 + var12 * var12;
-            this.err = var13;
-            this.radius = this.this$1.interpolateRadius(var9);
-            float var14 = var8 + this.radius;
-            float var15;
+            float nt = this.t + this.dt;
+            this.this$1.interpolatePosition(this.pos, nt);
+            float deltaX = pX - this.pos[0];
+            float deltaY = pY - this.pos[1];
+            float deltaZ = pZ - this.pos[2];
+            float d2 = deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ;
+            this.err = d2;
+            this.radius = this.this$1.interpolateRadius(nt); //MAY BE HERE
+            float avg2R = r + this.radius;
+            float sin2;
 
             if (this.calcDer)
             {
-                this.this$1.interpolateDerivative(this.der, var9);
+                this.this$1.interpolateDerivative(this.der, nt);
                 this.derLen = MathHelper.sqrt_float(this.der[0] * this.der[0] + this.der[1] * this.der[1] + this.der[2] * this.der[2]);
                 this.der[0] /= this.derLen;
                 this.der[1] /= this.derLen;
                 this.der[2] /= this.derLen;
-                var10 = -var7 * this.der[1] + var6 * this.der[2];
-                var11 = var7 * this.der[0] - var5 * this.der[2];
-                var12 = -var6 * this.der[0] + var5 * this.der[1];
-                var15 = var10 * var10 + var11 * var11 + var12 * var12;
-                this.err += var14 * var14 * var15;
+                deltaX = -dZ * this.der[1] + dY * this.der[2];
+                deltaY = dZ * this.der[0] - dX * this.der[2];
+                deltaZ = -dY * this.der[0] + dX * this.der[1];
+                sin2 = deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ;
+                this.err += avg2R * avg2R * sin2;
             }
 
-            var15 = var1 * var1;
+            sin2 = var1 * var1;
 
-            if (this.err > var15)
+            if (this.err > sin2)
             {
                 this.dt = (float)((double)this.dt * 0.6D);
             }
             else
             {
-                if (this.err >= var15 / 5.0F)
+                if (this.err >= sin2 / 5.0F)
                 {
                     this.t += this.dt;
                     return this.t < 0.5F;
                 }
 
+                if (this.radius == 0) {
+                	return false;
+                }
+                
+                if (Float.isNaN(this.radius)) {
+                	return false;
+                }
+                
                 this.dt = (float)((double)this.dt * 1.8D);
             }
         }
-        while (this.dt >= Math.ulp(this.t) * 2.0F);
+        while (this.dt >= Math.ulp(this.t) * 1.0F);
 
         throw new RuntimeException("CustomOreGen: Detected a possible infinite loop during bezier interpolation.  Please report this error.");
     }
